16fa8a6ec52742ee6bee5437f7424759cb5f1bfc
RUKtheCROOK <johngarcia053@gmail.com>
2023-01-22 12:27:04 -0800

    Class 3 code
    All the code added with class 3 and a little extra

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,70 @@
+let important = `<i class="nImportant fa-solid fa-ban"></i>`;
 function makeImportant() {
   console.log("makeImportant called");
   let hide = "hide";
+
   if ($("#I").hasClass(hide)) {
     $("#I").toggleClass(hide);
     $("#nI").toggleClass(hide);
+    important = `<i class="important fa-solid fa-circle-exclamation"></i>`;
   } else if ($("#nI").hasClass(hide)) {
     $("#nI").toggleClass(hide);
     $("#I").toggleClass(hide);
+    important = `<i class="nImportant fa-solid fa-ban"></i>`;
   }
 }
-
+// To hide the form
 function toggleForm() {
   console.log("toggleForm called");
   let hide = "hide";
   //   $("#task-form").toggleClass(hide);
   $("#task-form").toggle();
+}
+// To submit the form
+function submit() {
+  console.log("submit called");
+  let title = $("#txt-title").val();
+  let desc = $("#txt-desc").val();
+  let due = $("#sel-due").val();
+  let category = $("#sel-category").val();
+  let contact = $("#tel-contact").val();
+  let status = $("#sel-status").val();
+
+  let task = new Task(important, title, desc, due, category, contact, status);
+  displayTask(task);
+}
+
+function displayTask(task) {
+  // let table = `
+  // <table>
+  // <thead>
+  //   <tr>
+  //     <th class="tablenumber">Pet #</th>
+  //     <th class="tablename">Name</th>
+  //     <th class="tableage">Age</th>
+  //     <th class="tablegender">Gender</th>
+  //     <th class="tablepayment">Payment</th>
+  //     <th>Delete</th>
+  //   </tr>
+  //     </thead><tbody id="tableBody"></tbody></table>`;
+  let display = `
+  <div class="container flex-container-1">
+      <div>${task.important}</div>
+    <div>
+    <h3><u>${task.title}</u></h3>
+    <p id="description">${task.desc}</p>
+    </div>
+    <div><label>${task.due}</label>
+    <label>${task.status}</label>
+    </div>
+    <div><label>${task.category}</label>
+    <label>${task.contact}</label>
+    </div>
+    </div>
+  `;
+  $("#tasks").append(display);
+  // $("#tasks").append(table);
+  // $("#tableBody").append(display);
 }
 // Here is the Init function that will be called when the page loads
 function init() {
@@ -24,6 +74,7 @@ function init() {
   $("#nI").click(makeImportant);
   $("#I").click(makeImportant);
   $("#btn-list").click(toggleForm);
+  $("#btn-submit").click(submit);
 }
 
 window.onload = init;
---
+++ scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+  constructor(important, title, desc, due, category, contact, status) {
+    this.important = important;
+    this.title = title;
+    this.desc = desc;
+    this.due = due;
+    this.category = category;
+    this.contact = contact;
+    this.status = status;
+
+    this.name = "John";
+  }
+}
